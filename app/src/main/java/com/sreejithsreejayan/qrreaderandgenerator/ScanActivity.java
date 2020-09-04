2fb6d5541585669c1d722edd1883e479a21dc6a4
Sreejith <sreejithns2002@gmail.com>
2020-09-04 19:46:06 +0500

    flash feature in ScanActivity

--- app/src/main/java/com/sreejithsreejayan/qrreaderandgenerator/ScanActivity.java
+++ app/src/main/java/com/sreejithsreejayan/qrreaderandgenerator/ScanActivity.java
@@ -1,21 +1,21 @@
 package com.sreejithsreejayan.qrreaderandgenerator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.util.Log;
 import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
@@ -24,6 +24,7 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -33,6 +34,7 @@ public class ScanActivity extends AppCompatActivity {
     private static final int REQUEST_CAMERA_PERMISSION = 201;
     String intentData = "";
     Vibrator vibe;
+    private boolean isFlashOn=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,94 +52,173 @@ public class ScanActivity extends AppCompatActivity {
 
     private void initialiseDetectorsAndSources() {
 
-            Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
 
-            barcodeDetector = new BarcodeDetector.Builder(this)
-                    .setBarcodeFormats(Barcode.ALL_FORMATS)
-                    .build();
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
 
-            cameraSource = new CameraSource.Builder(this, barcodeDetector)
-                    .setRequestedPreviewSize(600,600)
-                    .setAutoFocusEnabled(true) //you should add this feature
-                    .build();
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(600, 600)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
 
-            surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-                @Override
-                public void surfaceCreated(SurfaceHolder holder) {
-                    try {
-                        if (ActivityCompat.checkSelfPermission(ScanActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                            cameraSource.start(surfaceView.getHolder());
-                        } else {
-                            ActivityCompat.requestPermissions(ScanActivity.this, new
-                                    String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(ScanActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(ScanActivity.this, new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+
+
+                    surfaceView.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            intentData = barcodes.valueAt(0).displayValue;
+                            startResultActivity();
+
+
                         }
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
+                    });
 
                 }
-
-                @Override
-                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                }
-
-                @Override
-                public void surfaceDestroyed(SurfaceHolder holder) {
-                    cameraSource.stop();
-                }
-            });
-
-
-            barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-                @Override
-                public void release() {
-                    Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void receiveDetections(Detector.Detections<Barcode> detections) {
-                    final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-                    if (barcodes.size() != 0) {
-
-
-                        surfaceView.post(new Runnable() {
-
-                            @Override
-                            public void run() {
-
-                                intentData = barcodes.valueAt(0).displayValue;
-                                startResultActivity();
-
-//                                }
-                            }
-                        });
-
-                    }
-                }
-            });
-        }
+            }
+        });
+    }
 
     private void startResultActivity() {
         vibe.vibrate(100);
-        Intent result = new Intent(ScanActivity.this,ResultActivity.class);
-        result.putExtra("resultString",intentData);
+        Intent result = new Intent(ScanActivity.this, ResultActivity.class);
+        result.putExtra("resultString", intentData);
         startActivity(result);
 
     }
 
 
     @Override
-        protected void onPause() {
-            super.onPause();
-            cameraSource.release();
+    protected void onPause() {
+        super.onPause();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseDetectorsAndSources();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_scan_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.torchAction:
+                try {
+                    setFlash();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            default:
+                Toast.makeText(ScanActivity.this, "someting went wrong", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
+    }
+
+    public static Camera getCamera(CameraSource cameraSource) {
+        Field[] declaredFields = CameraSource.class.getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            if (field.getType() == Camera.class) {
+                field.setAccessible(true);
+                try {
+                    Camera camera = (Camera) field.get(cameraSource);
+                    if (camera != null) {
+                        return camera;
+                    }
+
+                    return null;
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
         }
 
-        @Override
-        protected void onResume() {
-            super.onResume();
-            initialiseDetectorsAndSources();
+        return null;
+    }
+
+    public void setFlash() throws IOException {
+        getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        cameraSource.start(surfaceView.getHolder());
+        Camera _cam = getCamera(cameraSource);
+        if (_cam != null) {
+            if (isFlashOn){
+                Camera.Parameters _pareMeters = _cam.getParameters();
+                _pareMeters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                isFlashOn=!isFlashOn;
+                _cam.setParameters(_pareMeters);
+                _cam.startPreview();
+            }else {
+                Camera.Parameters _pareMeters = _cam.getParameters();
+                _pareMeters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                isFlashOn=!isFlashOn;
+                _cam.setParameters(_pareMeters);
+                _cam.startPreview();
+            }
         }
 
+    }
 }
