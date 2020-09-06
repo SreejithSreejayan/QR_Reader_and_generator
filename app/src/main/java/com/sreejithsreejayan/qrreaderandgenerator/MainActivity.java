1fe980385d3019fec7a0bb24696d03e8a53d2286
Sreejith <sreejithns2002@gmail.com>
2020-09-06 18:10:02 +0500

    exiting app only when back button pressed twice

--- app/src/main/java/com/sreejithsreejayan/qrreaderandgenerator/MainActivity.java
+++ app/src/main/java/com/sreejithsreejayan/qrreaderandgenerator/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button generateBtn,scanBtn,historyBtn;
+    boolean exitPressedOnce=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +48,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(MainActivity.this,HistoryActivity.class));
                 break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (exitPressedOnce){
+            finish();
+        }
+        this.exitPressedOnce=true;
+        Toast.makeText(MainActivity.this,"press again to exit",Toast.LENGTH_SHORT).show();
+
+        ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
+        Runnable runnable = new Runnable() {
+            public void run() {
+                exitPressedOnce=false;
+            }
+        };
+        worker.schedule(runnable, 2, TimeUnit.SECONDS);
     }
 }
